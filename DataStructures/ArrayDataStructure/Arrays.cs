ee059970f314235c475ac537d1b1965bfce1c2e2
Nirav Sanghadiya <nirav.sanghadiya8@gmail.com>
2023-07-30 20:27:12 +0530

    Find the maximum element from the array

---
+++ DataStructures/ArrayDataStructure/Arrays.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.ArrayDataStructure
+{
+    public static class Arrays
+    {
+        /* Find the largest element from the array
+          1.Brut force approach :
+             - Sort the array
+             - Get the last element of the array which is Max number 
+            Time : O(nlogn)
+            Space : O(1)
+          2. Optimized approach
+           - Create an variable maxElement and assign the first element of the array
+           - Check if the next element of the array is larger than the maxElement,If yes then assign
+             next element to maxElement. At the end, maxElement will have the highest integer
+         */
+        public static int GetMaxElementOfArray(int[] arr)
+        {
+            if (arr.Length == 0) return 0;
+
+            var maxElement = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > maxElement)
+                {
+                    maxElement = arr[i];
+                }
+            }
+            return maxElement;
+        }
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructures.ArrayDataStructure;
+
+
+var arr = new int[] { 1, 5, 2, 4, 7, 3 };
+var maxArrayItem = Arrays.GetMaxElementOfArray(arr);
+Console.WriteLine($"maximum element in the array is: {maxArrayItem}");
+
+
